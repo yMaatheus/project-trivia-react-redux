f0e0b50719c71cd3f08f93f1e0b4aa6cd090bf22
Gabriel Alves <gabrielalves@outlook.com>
2022-04-05 16:42:45 -0300

    Cria componente de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,68 @@
 import React from 'react';
 
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      playerDisabled: true,
+    };
+  }
+
+  isNameValid = () => {
+    const { name } = this.state;
+
+    return name.length > 0;
+  }
+
+  isEmailValid = () => {
+    const { email } = this.state;
+    const regexEmail = /[a-z0-9.]+@[a-z0-9]+\.[a-z]/i;
+
+    return regexEmail.test(email);
+  }
+
+  validateNameAndEmail = () => {
+    this.setState({ playerDisabled: !this.isNameValid() || !this.isEmailValid() });
+  }
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, this.validateNameAndEmail);
+  }
+
   render() {
-    return <h1>Login</h1>
+    const { playerDisabled } = this.state;
+
+    return (
+      <div>
+        <fieldset>
+          <input
+            data-testid="input-player-name"
+            label="Nome: "
+            type="text"
+            name="name"
+            onChange={ this.handleChange }
+          />
+          <input
+            data-testid="input-gravatar-email"
+            label="Email: "
+            type="email"
+            name="email"
+            onChange={ this.handleChange }
+          />
+          <button
+            data-testid="btn-play"
+            type="button"
+            label="Jogar"
+            disabled={ playerDisabled }
+          >
+            Jogar
+          </button>
+        </fieldset>
+      </div>
+    );
   }
 }
 
