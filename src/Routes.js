5ae282aac17a2798bb54d2a231a93f2690e422a9
Gabriel Alves <gabrielalves@outlook.com>
2022-04-05 16:00:20 -0300

    Cria pagina de login e configura routes

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Switch>
+        <Route path="/" exact component={ Login } />
+      </Switch>
+    );
+  }
+}
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store';
 
 ReactDOM.render(
-  <Provider store={ store }>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
