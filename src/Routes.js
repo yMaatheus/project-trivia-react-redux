d3d57d5a5ba6e752f1a8fb691b972b2cce654992
João Vitor Souza Rocha <jvitorsouza18@gmail.com>
2022-04-05 22:14:49 -0300

    req 2 erro no token

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import Game from './pages/Game';
 
 class Routes extends React.Component {
   render() {
     return (
       <Switch>
         <Route path="/" exact component={ Login } />
+        <Route path="/game" component={ Game } />
       </Switch>
     );
   }
--- src/reducers/player.js
+++ src/reducers/player.js
@@ -1,6 +1,23 @@
+import { SET_TOKEN } from '../actions';
+
 const FIRST_STATE = {
+  name: '',
+  assertions: '',
+  score: '',
+  gravatarEmail: '',
+  token: '',
 };
 
-const player = (state = FIRST_STATE) => state;
+const player = (state = FIRST_STATE, action) => {
+  switch (action.type) {
+  case SET_TOKEN:
+    return {
+      ...state,
+      token: action.payload,
+    };
+  default:
+    return state;
+  }
+};
 
 export default player;
