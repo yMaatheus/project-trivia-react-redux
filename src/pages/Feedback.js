4b1df7b991e3e6c9bafb217dbf6a2d746e6afec0
Letícia Cotta <leticiacotta@gmail.com>
2022-04-06 17:06:11 -0300

    Main Group 12: Requisito 12 Commitado TODO

---
+++ src/pages/Feedback.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+
+// class Feedback extends React.Component {
+//   render() {
+//     return (
+//     <fieldset data-testid="info-player">
+//       <header>
+//         <input
+//         data-testid="header-profile-picture"
+//         label="Imagem de Perfil"
+//         type="image"
+//         name="profile picture" 
+//         />
+//         <input
+//         data-testid="header-player-name"
+//         label="Jogador(a): "
+//         type="text"
+//         name="player"
+//         />
+//         <input
+//         data-testid="header-score"
+//         label="Pontuação: "
+//         type="number"
+//         name="score"
+//         />
+//       </header>
+//     </fieldset>
+//     );
+//   }
+// }
+
+// export default Feedback;
