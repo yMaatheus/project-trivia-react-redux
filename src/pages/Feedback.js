9296e6be868c61c0433d927cf1ebd5b13f7d6201
Letícia Cotta <leticiacotta@gmail.com>
2022-04-07 17:13:23 -0300

    Requisito 15 e descomentando 12

--- src/pages/Feedback.js
+++ src/pages/Feedback.js
@@ -1,32 +1,51 @@
-// import React from 'react';
+import React from 'react';
 
-// class Feedback extends React.Component {
-//   render() {
-//     return (
-//     <fieldset data-testid="info-player">
-//       <header>
-//         <input
-//         data-testid="header-profile-picture"
-//         label="Imagem de Perfil"
-//         type="image"
-//         name="profile picture" 
-//         />
-//         <input
-//         data-testid="header-player-name"
-//         label="Jogador(a): "
-//         type="text"
-//         name="player"
-//         />
-//         <input
-//         data-testid="header-score"
-//         label="Pontuação: "
-//         type="number"
-//         name="score"
-//         />
-//       </header>
-//     </fieldset>
-//     );
-//   }
-// }
+class Feedback extends React.Component {
+  constructor(){
+    super()
 
-// export default Feedback;
+    clickPlayAgainButton = () => {
+      const { saveToken, history } = this.props;
+      saveToken();
+      history.push('/');
+    }
+  } // Fim do constructor
+  render() {
+    return (
+    <fieldset data-testid="info-player">
+      <header>
+        <input
+        data-testid="header-profile-picture"
+        label="Imagem de Perfil"
+        type="image"
+        name="profile picture" 
+        />
+
+        <input
+        data-testid="header-player-name"
+        label="Jogador(a): "
+        type="text"
+        name="player"
+        />
+
+        <input
+        data-testid="header-score"
+        label="Pontuação: "
+        type="number"
+        name="score"
+        />
+
+        <button
+        data-testid="btn-play-again"
+        label="Play Again"
+        type="button"
+        name='button play again'
+        onClick={ this.clickPlayAgainButton }
+        />
+      </header>
+    </fieldset>
+    );
+  }
+}
+
+export default Feedback;
